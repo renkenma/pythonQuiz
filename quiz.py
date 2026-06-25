783a4b8010ffc551a984bdca8b444b575964b5ce
renkenma <marvinre@googlemail.com>
2026-06-25 11:47:38 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -20,7 +20,7 @@ if not st.session_state.quiz_started:
     st.markdown("""
     **Hey mein Schatz** 😊 Ich wünsche dir zum Geburtstag nur das Beste der Welt und dass all' deine Wünsche in Erfüllung gehen! 😊 Bei einigen dieser Wünsche spiele ich hoffentlich die Hauptrolle ❤️. 
     
-    Ich will dir natürlich soviel bieten wie möglich! Allerdings haben wir uns ein Limit bei den Geschenken gesetzt. Daher kann ich dich nicht zu allen 3 Ideen einladen, woran ich so gedacht habe 😔. Aber <span style="display:inline-block; text-align:center; vertical-align:middle; line-height:1;"><span style="display:block; border-bottom:1px solid currentColor; padding:0 2px;">∞</span><span style="display:block;">3</span></span> ist auch nicht gerade wenig! Aber ich will ja auch nicht, dass du nach Lust und Laune entscheidest und diese Entscheidung am Ende bereust! 
+    Ich will dir natürlich soviel bieten wie möglich! Allerdings haben wir uns ein Limit bei den Geschenken gesetzt. Daher kann ich dich nicht zu allen 3 Ideen einladen, woran ich so gedacht habe 😔. Das reduziert den Spaß nun auf ein Drittel, aber <span style="display:inline-block; text-align:center; vertical-align:middle; line-height:1;"><span style="display:block; border-bottom:1px solid currentColor; padding:0 2px;">∞</span><span style="display:block;">3</span></span> ist auch nicht gerade wenig! Aber ich will ja auch nicht, dass du nach Lust und Laune entscheidest und diese Entscheidung am Ende bereust! 
     
     Wie wäre es, wenn wir das machen, was am besten zu dir passt? Dir werden hier keine expliziten Erlebnisse vorgeschlagen. Stattdessen habe ich ein kleines Quiz für dich organisiert, das deinen Charakter prüft. 
     
